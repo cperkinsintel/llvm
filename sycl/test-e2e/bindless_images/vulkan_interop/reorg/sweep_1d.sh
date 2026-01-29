0c8aaeba013a2def9227ae02526e01a4175512db
Chris Perkins <chris.perkins@intel.com>
2026-01-29 13:21:47 -0800

    comment

--- sycl/test-e2e/bindless_images/vulkan_interop/reorg/sweep_1d.sh
+++ sycl/test-e2e/bindless_images/vulkan_interop/reorg/sweep_1d.sh
@@ -1,30 +1,30 @@
 # Unlike 2D, linear tiling seems to work fine for 1D images.
 
 
-echo "--- SAMPLED 1D.  OPTIMAL TILING ---"
+echo "--- SAMPLED 1D.  OPTIMAL & LINEAR TILING ---"
 ./vss_1d_test.bin 1025x
 ./vss_1d_test.bin 1025x --linear
 
-echo "--- SAMPLED 2D.  OPTIMAL TILING w SEMAPHORES ---"
+echo "--- SAMPLED 1D.  OPTIMAL & LINEAR TILING w SEMAPHORES ---"
 ./vss_1d_test.bin 1025x --semaphores
 ./vss_1d_test.bin 1025x --semaphores --linear
 
 ## SAMPLING is for READING. There is no sampled "write". 
 
-echo "--- UNSAMPLED 2D.  OPTIMAL TILING ---"
+echo "--- UNSAMPLED 1D.  OPTIMAL & LINEAR TILING ---"
 ./vsu_1d_test.bin 1025x
 ./vsu_1d_test.bin 1025x --linear
 
-echo "--- UNSAMPLED 2D.  OPTIMAL TILING w SEMAPHORES ---"
+echo "--- UNSAMPLED 1D.  OPTIMAL & LINEAR TILING w SEMAPHORES ---"
 ./vsu_1d_test.bin 1025x --semaphores
 ./vsu_1d_test.bin 1025x --semaphores --linear
 
 
-echo "--- UNSAMPLED 2D WRITE.  OPTIMAL TILING ---"
+echo "--- UNSAMPLED 1D WRITE.  OPTIMAL & LINEAR TILING ---"
 ./vsu_1d_w_test.bin 1025x
 ./vsu_1d_w_test.bin 1025x --linear
 
-echo "--- UNSAMPLED 2D WRITE.  OPTIMAL TILING w SEMAPHORES ---"
+echo "--- UNSAMPLED 1D WRITE.  OPTIMAL & LINEAR TILING w SEMAPHORES ---"
 ./vsu_1d_w_test.bin 1025x --semaphores
 ./vsu_1d_w_test.bin 1025x --semaphores --linear
 
